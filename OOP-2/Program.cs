20d9d52b1b06ff78fe31f4e5621e0bf7b9d5dd74
Maryam <mariamsaeedalshamy@gmail.com>
2026-09-20 14:17:13 +0400

    Delivery center class

--- OOP-2/Program.cs
+++ OOP-2/Program.cs
@@ -86,6 +86,59 @@
             }
         }
         #endregion
+
+        #region Delivery class
+        public class DeliveryCenter
+        {
+            private Shipment[] shipments;
+
+            public DeliveryCenter()
+            {
+                shipments = new Shipment[10];
+            }
+
+            public Shipment this[int index]
+            {
+                get
+                {
+                    if (index >= 0 && index < shipments.Length)
+                        return shipments[index];
+                    return default;
+                }
+                set
+                {
+                    if (index >= 0 && index < shipments.Length)
+                        shipments[index] = value;
+                }
+            }
+
+            public Shipment this[string trackingCode]
+            {
+                get
+                {
+                    for (int i = 0; i < shipments.Length; i++)
+                    {
+                        if (shipments[i] != null && shipments[i].TrackingCode == trackingCode)
+                            return shipments[i];
+                    }
+                    return default;
+                }
+            }
+
+            public bool AddShipment(Shipment shipment)
+            {
+                for (int i = 0; i < shipments.Length; i++)
+                {
+                    if (shipments[i] == null)
+                    {
+                        shipments[i] = shipment;
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        #endregion
         static void Main(string[] args)
         {
             
